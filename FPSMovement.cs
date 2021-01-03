24fba4f1177057df7292b0dc8b4806f5c9b2b7a4
DannyThicc <76801385+DannyThicc@users.noreply.github.com>
2021-01-03 15:29:40 +0800

    Add files via upload

---
+++ FPSMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+
+    public float Normalspeed = 10f;
+    public float gravity = -9.81f;
+    public float groundDistance = 0.4f;
+    public float jumpHeight = 3f;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+
+    private void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * Normalspeed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Normalspeed = 29f;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            Normalspeed = 10f;
+        }
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
